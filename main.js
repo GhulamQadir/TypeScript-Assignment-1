b3aba1cf459fb44d305c49e4ff5d4ba963b9ff9a
Ghulam Qadir <hafizghulamqadir.2005@gmail.com>
2024-02-16 18:06:29 +0500

    more questions uploaded

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Q1
 // let personName1 = "John";
-// console.log(`Hello ${a}, would you like to learn some Typescript today? `)
+// console.log(`Hello ${personName1}, would you like to learn some Typescript today? `)
 // Q2
 // let personName2 = "AlI"
 // let lower = personName2.toLowerCase();
@@ -47,7 +47,7 @@
 // console.log(`I would like to better the situation of ${transport[1]}s of my country`)
 // console.log(`I would like to go to Turkey by ${transport[2]}`)
 // Q:14 & Q:15 
-var guestList = ["Mohsin", "Usama", "Ayan"];
+// let guestList = ["Mohsin", "Usama", "Ayan"]
 // for (var j = 0; j < guestList.length; j++) {
 //     console.log(`${guestList[j]}, You are invited to dinner at our house on 14th August.`)
 // }
@@ -58,10 +58,10 @@ var guestList = ["Mohsin", "Usama", "Ayan"];
 // }
 // console.log("We have found a bigger dinner table, so we're inviting more people!")
 // Q:16
-guestList.unshift("Shahbaz");
+// guestList.unshift("Shahbaz")
 // console.log(guestList)
-guestList.splice(1, 1, "Sameer");
-guestList.push("Sohail");
+// guestList.splice(1, 1, "Sameer")
+// guestList.push("Sohail")
 // for (var j = 0; j < guestList.length; j++) {
 //     console.log(`${guestList[j]}, You are invited to dinner at our house on 14th August.`)
 // }
@@ -96,14 +96,108 @@ var places = ["Turkey", "Dubai", "Northern Areas", "Islamabad", "Singapore"];
 // let tsObj = { "name": "Ghulam Qadir", "rollNo": 3235, "qualification": "Matric" }
 // console.log(tsObj)
 // Q:23
-var age = 15;
-console.log(age < 18 ? "You can't enjoy this ride" : "You can enjoy this ride");
-var trafficLight = "red";
-console.log(trafficLight === "red" ? "Stop" : "Go");
-var num = 6;
-console.log(num % 2 === 0 ? "Even" : "Odd");
-var checkVar = "testing";
-console.log(typeof (checkVar) === "string" ? "String type variabe" : "Num type variable");
-var num1 = 3;
-var num2 = 4;
-console.log(num1 > num2 ? "Num1 is greater than Num2" : "Num2 is greater than Num1");
+// let age = 15;
+// console.log(age < 18 ? "You can't enjoy this ride" : "You can enjoy this ride")
+// let trafficLight = "red";
+// console.log(trafficLight === "red" ? "Stop" : "Go")
+// let num = 6
+// console.log(num % 2 === 0 ? "Even" : "Odd")
+// let checkVar = "testing"
+// console.log(typeof (checkVar) === "string" ? "String type variabe" : "Num type variable")
+// let num1 = 3;
+// let num2 = 4;
+// console.log(num1 > num2 ? "Num1 is greater than Num2" : "Num2 is greater than Num1")
+// let string1 = "Typerscript"
+// let string2 = "typerscript"
+// console.log(string1 === string2 ? "True" : "False")
+// let gender = "Male"
+// let age = 20
+// if ((gender === "Male" && age >= 15) || (gender === "Female" && age >= 18)) {
+//     console.log("You can enjoy that ride.")
+// }
+// else {
+//     console.log("Your age is less")
+// }
+// let num = 36
+// if (num % 2 === 0) {
+//     console.log("even num")
+// }
+// else {
+//     console.log("odd num")
+// }
+// CHECK ARRAY
+// let a = "Karachi"
+// let b = ["Lahore", 1, false]
+// console.log(Array.isArray(a))
+// console.log(Array.isArray(b))
+// Q:25
+var alien_color = "red";
+// if (alien_color === "green") {
+//     console.log("The player just earned 5 points")
+// }
+// Q:26
+// if (alien_color === "green") {
+//     console.log("The player just earned 5 points")
+// }
+// else {
+//     console.log("The player just earned 10 points")
+// }
+// Q:27
+// if (alien_color === "green") {
+//     console.log("The player just earned 5 points")
+// }
+// else if (alien_color === "yellow") {
+//     console.log("The player just earned 10 points")
+// }
+// else if (alien_color === "red") {
+//     console.log("The player just earned 15 points")
+// }
+// Q:28
+// let age = 15
+// if (age < 2) {
+//     console.log("The person is a baby")
+// }
+// else if (age < 4) {
+//     console.log("The person is a toddler")
+// }
+// else if (age < 13) {
+//     console.log("The person is a kid")
+// }
+// else if (age < 20) {
+//     console.log("The person is a teenager")
+// }
+// else if (age < 65) {
+//     console.log("The person is an adult")
+// }
+// else if (age >= 65) {
+//     console.log("The person is an elder")
+// }
+// Q:29
+// let favorite_fruits = ["mango", "apple", "banana"]
+// for (var i = 0; i < favorite_fruits.length; i++) {
+//     if (favorite_fruits[i] === "banana") {
+//         console.log(`You really like ${favorite_fruits[i]}s`)
+//     }
+//     if (favorite_fruits[i] === "orange") {
+//         console.log(`You really like ${favorite_fruits[i]}s`)
+//     }
+//     if (favorite_fruits[i] === "apple") {
+//         console.log(`You really like ${favorite_fruits[i]}s`)
+//     }
+//     if (favorite_fruits[i] === "gava") {
+//         console.log(`You really like ${favorite_fruits[i]}s`)
+//     }
+//     if (favorite_fruits[i] === "pineapple") {
+//         console.log(`You really like ${favorite_fruits[i]}s`)
+//     }
+// }
+// Q:30
+var userNames = ["Eric", "John", "Clark", "Admin", "Jack"];
+for (var i = 0; i < userNames.length; i++) {
+    if (userNames[i] === "Admin") {
+        console.log("Hello ".concat(userNames[i], ", would you like to see a status report"));
+    }
+    if (userNames[i] !== "Admin") {
+        console.log("Hello ".concat(userNames[i], ", thank you for logging in again"));
+    }
+}
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 // Q1
 // let personName1 = "John";
-// console.log(`Hello ${a}, would you like to learn some Typescript today? `)
+// console.log(`Hello ${personName1}, would you like to learn some Typescript today? `)
 
 
 // Q2
@@ -74,7 +74,7 @@
 
 
 // Q:14 & Q:15 
-let guestList = ["Mohsin", "Usama", "Ayan"]
+// let guestList = ["Mohsin", "Usama", "Ayan"]
 // for (var j = 0; j < guestList.length; j++) {
 //     console.log(`${guestList[j]}, You are invited to dinner at our house on 14th August.`)
 // }
@@ -88,11 +88,11 @@ let guestList = ["Mohsin", "Usama", "Ayan"]
 // console.log("We have found a bigger dinner table, so we're inviting more people!")
 
 // Q:16
-guestList.unshift("Shahbaz")
+// guestList.unshift("Shahbaz")
 // console.log(guestList)
 
-guestList.splice(1, 1, "Sameer")
-guestList.push("Sohail")
+// guestList.splice(1, 1, "Sameer")
+// guestList.push("Sohail")
 
 // for (var j = 0; j < guestList.length; j++) {
 //     console.log(`${guestList[j]}, You are invited to dinner at our house on 14th August.`)
@@ -145,18 +145,135 @@ let places = ["Turkey", "Dubai", "Northern Areas", "Islamabad", "Singapore"]
 
 
 // Q:23
-let age = 15;
-console.log(age < 18 ? "You can't enjoy this ride" : "You can enjoy this ride")
+// let age = 15;
+// console.log(age < 18 ? "You can't enjoy this ride" : "You can enjoy this ride")
 
-let trafficLight = "red";
-console.log(trafficLight === "red" ? "Stop" : "Go")
+// let trafficLight = "red";
+// console.log(trafficLight === "red" ? "Stop" : "Go")
 
-let num = 6
-console.log(num % 2 === 0 ? "Even" : "Odd")
+// let num = 6
+// console.log(num % 2 === 0 ? "Even" : "Odd")
 
-let checkVar = "testing"
-console.log(typeof (checkVar) === "string" ? "String type variabe" : "Num type variable")
+// let checkVar = "testing"
+// console.log(typeof (checkVar) === "string" ? "String type variabe" : "Num type variable")
 
-let num1 = 3;
-let num2 = 4;
-console.log(num1 > num2 ? "Num1 is greater than Num2" : "Num2 is greater than Num1")
+// let num1 = 3;
+// let num2 = 4;
+// console.log(num1 > num2 ? "Num1 is greater than Num2" : "Num2 is greater than Num1")
+
+
+// let string1 = "Typerscript"
+// let string2 = "typerscript"
+
+// console.log(string1 === string2 ? "True" : "False")
+
+
+// let gender = "Male"
+// let age = 20
+// if ((gender === "Male" && age >= 15) || (gender === "Female" && age >= 18)) {
+//     console.log("You can enjoy that ride.")
+// }
+// else {
+//     console.log("Your age is less")
+// }
+
+// let num = 36
+// if (num % 2 === 0) {
+//     console.log("even num")
+// }
+// else {
+//     console.log("odd num")
+// }
+
+
+// CHECK ARRAY
+// let a = "Karachi"
+// let b = ["Lahore", 1, false]
+// console.log(Array.isArray(a))
+// console.log(Array.isArray(b))
+
+
+
+// Q:25
+let alien_color = "red"
+// if (alien_color === "green") {
+//     console.log("The player just earned 5 points")
+// }
+
+// Q:26
+// if (alien_color === "green") {
+//     console.log("The player just earned 5 points")
+// }
+// else {
+//     console.log("The player just earned 10 points")
+// }
+
+
+// Q:27
+// if (alien_color === "green") {
+//     console.log("The player just earned 5 points")
+// }
+// else if (alien_color === "yellow") {
+//     console.log("The player just earned 10 points")
+// }
+// else if (alien_color === "red") {
+//     console.log("The player just earned 15 points")
+// }
+
+
+// Q:28
+// let age = 15
+// if (age < 2) {
+//     console.log("The person is a baby")
+// }
+// else if (age < 4) {
+//     console.log("The person is a toddler")
+// }
+// else if (age < 13) {
+//     console.log("The person is a kid")
+// }
+// else if (age < 20) {
+//     console.log("The person is a teenager")
+// }
+// else if (age < 65) {
+//     console.log("The person is an adult")
+// }
+// else if (age >= 65) {
+//     console.log("The person is an elder")
+// }
+
+
+
+// Q:29
+// let favorite_fruits = ["mango", "apple", "banana"]
+// for (var i = 0; i < favorite_fruits.length; i++) {
+//     if (favorite_fruits[i] === "banana") {
+//         console.log(`You really like ${favorite_fruits[i]}s`)
+//     }
+//     if (favorite_fruits[i] === "orange") {
+//         console.log(`You really like ${favorite_fruits[i]}s`)
+//     }
+//     if (favorite_fruits[i] === "apple") {
+//         console.log(`You really like ${favorite_fruits[i]}s`)
+//     }
+//     if (favorite_fruits[i] === "gava") {
+//         console.log(`You really like ${favorite_fruits[i]}s`)
+//     }
+//     if (favorite_fruits[i] === "pineapple") {
+//         console.log(`You really like ${favorite_fruits[i]}s`)
+//     }
+// }
+
+
+
+// Q:30
+let userNames = ["Eric", "John", "Clark", "Admin", "Jack"]
+for (var i = 0; i < userNames.length; i++) {
+    if (userNames[i] === "Admin") {
+        console.log(`Hello ${userNames[i]}, would you like to see a status report`)
+    }
+    if (userNames[i] !== "Admin") {
+        console.log(`Hello ${userNames[i]}, thank you for logging in again`)
+    }
+}
+
